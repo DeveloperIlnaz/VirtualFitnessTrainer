85d5cb27d88574f9b593df751a306d2c9e6b51d2
Ильназ Хайруллин <sport-ilnaz@mail.ru>
2019-08-23 16:16:27 +0300

    Revert "v1"
    This reverts commit 5e3e62e10fdc9a06519896bb7938e35cd1aeec52.

---
+++ VirtualFitnessTrainer.Test/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VirtualFitnessTrainer.Test
+{
+    public class User
+    {
+        public List<User> Users { get; set; } = new List<User>();
+
+        public static void Add(string login, string password)
+        {
+            
+        }
+    }
+}
